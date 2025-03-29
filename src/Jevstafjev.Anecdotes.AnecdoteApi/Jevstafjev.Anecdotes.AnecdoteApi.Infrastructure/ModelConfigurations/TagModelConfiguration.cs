63f9237b27448190326f83057e16e5eaa596ae9f
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 18:38:15 +0200

    Create model configurations

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure/ModelConfigurations/TagModelConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure.ModelConfigurations;
+
+public class TagModelConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.ToTable("Tags");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).IsRequired();
+        builder.Property(x => x.Name).HasMaxLength(128).IsRequired();
+
+        builder.HasMany(x => x.Anecdotes);
+    }
+}
