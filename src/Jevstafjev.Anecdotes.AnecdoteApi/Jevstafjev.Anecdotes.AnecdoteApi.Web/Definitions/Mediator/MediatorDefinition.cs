18fa77175768d94b1dcffffff5bba53a8243a169
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 21:09:20 +0200

    Add MediatR

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Definitions/Mediator/MediatorDefinition.cs
@@ -0,0 +1,11 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.Mediator;
+
+public class MediatorDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+    }
+}
