aa9b6d1f222f14c63010628981a07dda940b664b
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 19:42:03 +0200

    Add database connection

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Definitions/DbContext/DbContextDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure;
+using Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.DbContext;
+
+public class DbContextDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString(nameof(ApplicationDbContext));
+            options.UseSqlServer(connectionString);
+        });
+    }
+}
