cf6142435fcc6685c5b089690ea4fcfe20910b78
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-29 18:36:40 +0200

    Add DbContext

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+using Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Infrastructure;
+
+public class ApplicationDbContext : DbContextBase
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Anecdote> Anecdotes { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Anecdote>().Navigation(x => x.Tags).AutoInclude();
+    }
+}
