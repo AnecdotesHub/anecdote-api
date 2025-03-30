86afcf116935e3310cf75624e4e83a955cb31a77
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-30 22:46:45 +0200

    Create get tags list endpoint

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Application/Messaging/TagMessages/Queries/TagGetListRequest.cs
@@ -0,0 +1,28 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using Ardalis.Result;
+using AutoMapper;
+using Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+using Jevstafjev.Anecdotes.AnecdoteApi.Web.Application.Messaging.TagMessages.ViewModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Application.Messaging.TagMessages.Queries;
+
+public record TagGetListRequest : IRequest<Result<List<TagViewModel>>>;
+
+public class TagGetListRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    : IRequestHandler<TagGetListRequest, Result<List<TagViewModel>>>
+{
+    public Task<Result<List<TagViewModel>>> Handle(TagGetListRequest request, CancellationToken cancellationToken)
+    {
+        var repository = unitOfWork.GetRepository<Tag>();
+
+        var entities = repository.GetAll()
+            .Include(x => x.Anecdotes)
+            .IgnoreAutoIncludes()
+            .OrderBy(x => x.Name);
+
+        var mapped = mapper.Map<List<TagViewModel>>(entities);
+        return Task.FromResult(Result.Success(mapped));
+    }
+}
---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Endpoints/TagEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+using Jevstafjev.Anecdotes.AnecdoteApi.Web.Application.Messaging.TagMessages.Queries;
+using Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.Base;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Endpoints;
+
+public class TagEndpoints : AppDefinition
+{
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.MapTagEndpoints();
+    }
+}
+
+internal static class TagEndpointsExtensions
+{
+    public static void MapTagEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/tags/").WithTags(nameof(Tag));
+
+        group.MapGet("get-list", async ([FromServices] IMediator mediator, HttpContext context) =>
+            await mediator.Send(new TagGetListRequest(), context.RequestAborted))
+            .Produces(200)
+            .WithOpenApi();
+    }
+}
