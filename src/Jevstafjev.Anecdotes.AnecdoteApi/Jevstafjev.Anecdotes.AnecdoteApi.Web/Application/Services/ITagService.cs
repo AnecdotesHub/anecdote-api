53c55eb9a5278393023d86596602658d3ec1ce9a
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-03-30 21:51:47 +0200

    Create TagService

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Application/Services/ITagService.cs
@@ -0,0 +1,30 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Application.Services;
+
+public interface ITagService
+{
+    Task<TagsAdditionResult> AddTagsAsync(Anecdote entity, List<string> tags);
+}
+
+public readonly struct TagsAdditionResult
+{
+    public TagsAdditionResult(List<string> toCreate, List<string> toDelete)
+    {
+        ToCreate = toCreate;
+        ToDelete = toDelete;
+    }
+
+    public TagsAdditionResult(Exception exception)
+    {
+        Exception = exception;
+    }
+
+    public List<string>? ToCreate { get; }
+
+    public List<string>? ToDelete { get; }
+
+    public Exception? Exception { get; }
+
+    public bool IsSuccess => Exception is null;
+}
---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Web/Definitions/DependencyContainer/ContainerDefinition.cs
@@ -0,0 +1,12 @@
+﻿using Jevstafjev.Anecdotes.AnecdoteApi.Web.Application.Services;
+using Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.AnecdoteApi.Web.Definitions.DependencyContainer;
+
+public class ContainerDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<ITagService, TagService>();
+    }
+}
