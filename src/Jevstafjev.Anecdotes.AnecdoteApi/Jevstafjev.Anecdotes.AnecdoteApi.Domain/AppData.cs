6342452327324b4be266809e905cf86623c1ef77
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 12:54:22 +0300

    Add authentication

---
+++ src/Jevstafjev.Anecdotes.AnecdoteApi/Jevstafjev.Anecdotes.AnecdoteApi.Domain/AppData.cs
@@ -0,0 +1,8 @@
+﻿namespace Jevstafjev.Anecdotes.AnecdoteApi.Domain;
+
+public class AppData
+{
+    public const string DefaultPolicyName = "Administrator";
+
+    public const string AdministratorRoleName = "Administrator";
+}
